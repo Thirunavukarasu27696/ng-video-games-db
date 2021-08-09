3576b590b228d52a2d6f8ee68284989249b275df
Thirunavukarasu s <thirunavukarasu27696@gmail.com>
2021-08-09 11:46:16 +0530

    initial commir

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -12,11 +12,11 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     req = req.clone({
       setHeaders: {
-        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-key': '94e42ec718msh856f2e7456077fcp1c36bfjsn9b76b910cad2',
         'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
       setParams: {
-        key: 'e40e743af2c94b0c916a8aa618fb4473',
+        key: 'b3efa331b5bc4b8eaaff3092c1687086',
       }
     });
     return next.handle(req);
